c95e719105cef80a6aa7f9612993f296c0089f36
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-03-17 00:41:59 +0000

    task 4 rectified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-int main(void){
+/*the main function is the starting point of the program with no arguments in our case*/
+int main(void)
+	{
 
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 
-}
+	}
