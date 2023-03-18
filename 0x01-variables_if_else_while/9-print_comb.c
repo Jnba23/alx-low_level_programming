df5ec71fe47917b50aecd14fb1c50ecdb2928af9
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-03-18 15:43:11 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,21 +7,13 @@
 int main(void)
 {
 	int d1 = 48;
-	int d2 = 48;
 
-	while (d1 <= 57 && d2 <=57)
+	while (d1 <= 57)
 	{
-		if (d1 == d2)
-		{
-		continue;
-		d2++;
-		}
 		putchar (d1);
-		putchar (d2);
 		putchar (',');
 		putchar (' ');
 		d1++;
-		d2++;
 	}
 	return (0);
 }
