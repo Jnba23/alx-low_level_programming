753e9eac57aea4df6edbc3c28acf02b00e0fa1a1
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-03-18 01:01:29 +0000

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter = 97; 
+
+	while (letter <= 122 && letter != 113, 101)
+	{
+	putchar(letter);
+	letter++;
+	}
+}
