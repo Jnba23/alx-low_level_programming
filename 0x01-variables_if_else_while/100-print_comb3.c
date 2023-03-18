e409a1f93407e9895e26d99aba4e93ee16481709
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-03-18 16:55:25 +0000

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int d1 = 48;
+	int d2 = 48;
+
+	for (d1 = 48; d1 <= 57; d1++)
+	{
+		putchar (d1);
+		if (d1 <= d2)
+		{
+		continue;
+		d2++;
+		} else
+		{
+		putchar(d2);
+		putchar(',');
+		putchar(' ');
+		d2++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
