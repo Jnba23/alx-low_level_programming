2bd7eb7a04dd41b3fa9e102e6d9794248c849396
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-03-20 21:24:20 +0000

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int d1, d2, d3, d4;
+
+	for (d1 = 48; d1 <= 57; d1++)
+	{
+		for (d2 = 48; d2 <= 57; d2++)
+		{
+			for (d3 = 47; d3 <= 57; d3++)
+			{
+				for ( d4 = 48; d4 <= 57; d4++)
+				{
+					if ((d1 + d2) < (d3 + d4))
+					{
+						putchar(d1);
+						putchar(d2);
+						putchar(' ');
+						putchar(d3);
+						putchar(d4);
+						if ((d1+d2) != 113 || (d3 + d4) != 114)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}	
+	putchar('\n');
+	return (0);
+}
