204e55af40b82ab13402a0d4b7022dff2c4648cc
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-03-22 01:59:59 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _islower- Entry point
+ *
+ * Return: Always 0 (Success)
+ * c ; variable of type int
+*/
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
