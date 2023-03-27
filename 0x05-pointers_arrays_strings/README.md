a9286b119a1dcaa82a509e2e9d6b3d84d06757ec
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-03-27 21:21:50 +0000

    task 0

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+- tasks 0 : the function takes a pointer to an int as parameter and updates the value it points to to 98
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+void reset_to_98(int *n);
+#endif
