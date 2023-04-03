bbf30d18a49e133348ff466abaf1d50f224dfc29
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-04-03 11:54:06 +0000

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,16 +7,16 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char lowUpp[] = "ABCDEFJHIJKLMNOPQRSTUVWXYZabcsdefjhjklmnopqrstuvwxyz";
+	char lowUpp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (*(s + i) == *(lowUpp + j))
+			if (s[i] == lowUpp[j])
 			{
-				*(s + i) = *(rot13 + j);
+				s[i] = rot13[j];
 				break;
 			}
 		}
