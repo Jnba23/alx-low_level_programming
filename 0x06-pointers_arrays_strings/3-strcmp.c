46bae348d53b9e87ddb19e93cb66627ff2d77408
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-04-03 01:39:00 +0000

    task 7

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,15 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int comp = 0, compT = 0;
+	int comp = 0;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
 			comp = ((int) s1[i] - 48) - ((int) s2[i] - 48);
-			compT += comp;
-			break;
+		break;
 		}
 	}
-	return (compT);
+	return (comp);
 }
