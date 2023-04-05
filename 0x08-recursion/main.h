2672596b0151823611772bb6387021cbee2f5b4b
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-04-05 22:22:16 +0000

    task 0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+void _puts_recursion(char *s);
+int _putchar(char c);
+#endif
