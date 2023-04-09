f93db06fa3bfddb751904d48234b0ed85090a48c
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-04-09 08:36:40 +0000

    task 1 rectified

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -2,7 +2,9 @@
 # to compile all library codes (*.c) into object file //
 gcc -c -Wall -Werror -Wextra *.c
 
-# to bundle all object files within the current directory into one indexated static library liball.a
+# to bundle all object files within the current directory into one static library liball.a
+ar rc liball.a *.c
 
-ar rcs liball.a *.c
+# to indexate the library
+ranlib liball.a
 
