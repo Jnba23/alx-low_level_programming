cc846cd413191b86d2da6489fd8a3feee182d98b
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-04-10 12:43:09 +0000

    task 0

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _putchar(char c);
+int main(int argc, char* argv[]);
+#endif
