e6ced59a617c15c263783561d917297659e078aa
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-04-13 22:23:52 +0000

    task 0

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif
