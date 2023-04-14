70dff9917b044f8141853303fe9a934d7420fb5f
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-04-14 00:46:08 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,32 +11,29 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i, j, l1;
+	char *result;
+	unsigned int s1_len, s2_len, result_len, i, j;
 
-	l1 = strlen(s1);
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	s = malloc((l1 + n + 1) * sizeof(char));
-	if (s == NULL)
-	{
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+
+	if (n >= s2_len)
+		n = s2_len;
+	result_len = s1_len + n;
+	result = malloc(result_len + 1);
+
+	if (result == NULL)
 		return (NULL);
-	}
-	/* copying the first string to s */
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		s[i] = s1[i];
-	}
-	for (j = 0; j < n && s2[j] != '\0'; j++)
-	{
-		s[i + j] = s2[j];
-	}
-	s[i + j] = '\0';
-	return (s);
+	for (i = 0; i < s1_len; i++)
+		result[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		result[i + j] = s2[j];
+
+	result[i + j] = '\0';
+	return (result);
 }
