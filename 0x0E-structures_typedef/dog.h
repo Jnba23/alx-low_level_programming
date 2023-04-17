f8901244a7241819306a0c9c7f62742364ed2b0e
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-04-17 13:08:37 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog structure
+ * @name: neme of the dog
+ * @age: age of the dog
+ * @owner: of the dog
+ *
+ * Description: it's a dog struct
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
