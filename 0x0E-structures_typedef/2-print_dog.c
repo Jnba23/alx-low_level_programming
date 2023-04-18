156b630454b763354c5914849bf0baf00f96ad0e
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-04-18 22:02:55 +0000

    task 2 rectified

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	else
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		printf("age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
