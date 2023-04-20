e21002cdc258960e514a317f10a89606d0f633a7
abdelwadoud <sayadabdelwadoud@gmail.com>
2023-04-20 23:18:31 +0000

    task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
+
